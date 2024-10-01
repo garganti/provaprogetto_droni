8b546fa65610bfc39c394d11647b16dd6279afa1
Angelo Gargantini <angelo.gargantini@unibg.it>
2024-10-01 16:08:26 +0200

    Update App.java

--- codice/dronefly/provadroni/src/main/java/ingdelsw/provadroni/App.java
+++ codice/dronefly/provadroni/src/main/java/ingdelsw/provadroni/App.java
@@ -4,6 +4,9 @@ package ingdelsw.provadroni;
  * Hello world!
  */
 public class App {
+    
+    static int F = 10;
+    
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello World!");
